b73f4a297251766b1fecc0240ab74ab07930d9e8
pedrobvferreira <pedrobvf@hotmail.com>
2022-03-23 19:52:20 +0000

    fix

--- personTest/src/main/resources/data.sql
+++ personTest/src/main/resources/data.sql
@@ -4,4 +4,6 @@ id INT AUTO_INCREMENT  PRIMARY KEY,
 name VARCHAR(50) NOT NULL,  
 age INT(8) NOT NULL,
 email VARCHAR(100) NOT NULL
-);  
+);
+
+insert into person(id, name, age, email) values (1, 'pedro', 17, 'pedrobvf@hotmail.com');
