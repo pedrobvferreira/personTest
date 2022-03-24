5021e856776b5b24f31d41ea3ed1609653a63ceb
pedrobvferreira <pedrobvf@hotmail.com>
2022-03-24 18:39:35 +0000

    junit

--- helloTest/src/test/java/com/ht/hello/test/HelloControllerTest.java
+++ helloTest/src/test/java/com/ht/hello/test/HelloControllerTest.java
@@ -6,8 +6,9 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
@@ -20,8 +21,13 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import com.ht.hello.controller.HelloController;
 import com.ht.hello.service.HelloService;
 
-@RunWith(SpringJUnit4ClassRunner.class)
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
 public class HelloControllerTest {
+	
+	@Autowired
 	private MockMvc mockMvc;
 
 	@Mock
