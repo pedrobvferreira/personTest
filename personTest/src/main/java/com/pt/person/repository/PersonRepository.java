07add2158bae63ce2f9680914eef1668244070c7
pedrobvferreira <pedrobvf@hotmail.com>
2022-03-28 11:23:35 +0100

    @DataJpaTest

--- personTest/src/main/java/com/pt/person/repository/PersonRepository.java
+++ personTest/src/main/java/com/pt/person/repository/PersonRepository.java
@@ -1,11 +1,19 @@
 package com.pt.person.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.pt.person.model.Person;
 
 @Repository
-public interface PersonRepository extends JpaRepository<Person, Integer>{
-
+public interface PersonRepository extends JpaRepository<Person, Integer> {
+	
+	Person findByPhoneNumber(String phoneNumber);
+	
+	@Query(value = "select * from person as u where u.first_name like :name%", nativeQuery = true)
+	List<Person> findByNameLike(@Param("name") String name);
 }
