dff727120309633253fd48065997a870a104bb9f
Pedro Ferreira <pedrobvf@hotmail.com>
2024-09-27 17:21:35 +0000

    fixes

--- src/main/java/com/pt/person/DemoApplication.java
+++ src/main/java/com/pt/person/DemoApplication.java
@@ -3,10 +3,22 @@ package com.pt.person;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.sql.SQLException;
+
+import org.h2.tools.Server;
+
 @SpringBootApplication
 public class DemoApplication {
 
   public static void main(String[] args) {
+
+    try {
+      // Iniciar o servidor H2 com o console habilitado para conexões remotas
+      Server.createWebServer("-web", "-webAllowOthers", "-tcpAllowOthers", "-browser").start();
+    } catch (SQLException e) {
+        e.printStackTrace();
+    }
+
     SpringApplication.run(DemoApplication.class, args);
   }
 
