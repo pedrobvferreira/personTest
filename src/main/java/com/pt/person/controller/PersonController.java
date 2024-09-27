d4fbc4606cabf81147a54b1968d2df064cb96c95
Pedro Ferreira <pedrobvf@hotmail.com>
2024-09-27 17:58:17 +0100

    Update PersonController.java

--- src/main/java/com/pt/person/controller/PersonController.java
+++ src/main/java/com/pt/person/controller/PersonController.java
@@ -13,7 +13,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.pt.person.dto.PersonDTO;
-import com.pt.person.model.Person;
 import com.pt.person.service.PersonService;
 
 @RestController
