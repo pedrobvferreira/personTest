d9bf66b47c21e02817928ffab88473a51e83cac6
Pedro Ferreira <pedrobvf@hotmail.com>
2024-09-30 10:06:45 +0000

    fix test

--- src/test/java/com/pt/person/controller/PersonControllerTest.java
+++ src/test/java/com/pt/person/controller/PersonControllerTest.java
@@ -21,7 +21,6 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.pt.person.controller.PersonController;
 import com.pt.person.dto.PersonDTO;
 import com.pt.person.service.PersonService;
 
